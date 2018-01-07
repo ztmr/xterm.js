fea30980560103ffc5aef94caa10ad4d87be7326
Tomas Morstein <tmr@idea.cz>
2018-01-07 21:09:20 +0000

    add boot script

--- idea-boot.sh
+++ idea-boot.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ `id -un` != "idea" ] ; then
+  echo "Must be run as 'idea' user!"
+  exit 1;
+fi
+
 export XTERM_ENTRYPOINT=/opt/gtmis/bin/tied
 cd /data/xterm
 nohup node demo/app &
